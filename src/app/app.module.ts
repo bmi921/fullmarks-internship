1784bb021bce16cb1123fa4bcd5ad7f6b2b9a553
bmi921 <bmi921gm@gmail.com>
2025-09-08 14:23:29 +0900

    feat: header, side-menuのUI実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SideMenuComponent } from './shared/side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    HeaderComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule, 
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
