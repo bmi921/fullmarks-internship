469db954a0f2c564521a17b4fdf06acade008ab1
bmi921 <bmi921gm@gmail.com>
2025-09-08 17:46:19 +0900

    feat: カード表示まで実装した

--- src/app/pages/quiz/quiz.module.ts
+++ src/app/pages/quiz/quiz.module.ts
@@ -1,17 +1,13 @@
+import { QuizCardComponent } from 'src/app/component/quiz-card/quiz-card.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { QuizRoutingModule } from './quiz-routing.module';
 import { QuizComponent } from './quiz.component';
 
-
 @NgModule({
-  declarations: [
-    QuizComponent
-  ],
-  imports: [
-    CommonModule,
-    QuizRoutingModule
-  ]
+  declarations: [QuizComponent, QuizCardComponent],
+  imports: [CommonModule, QuizRoutingModule, FormsModule],
 })
-export class QuizModule { }
+export class QuizModule {}
