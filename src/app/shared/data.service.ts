9d5c052e0b7189ec6f1d9253b960aadac1aab5c0
bmi921 <bmi921gm@gmail.com>
2025-09-09 15:58:00 +0900

    feat: 正解した/不正解した問題の一覧表示を追加

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -14,4 +14,15 @@ export class DataService {
   getCharacters(): Observable<Character[]> {
     return this.http.get<Character[]>(this.dataUrl);
   }
+
+  getAnswerLog(type: 'correct' | 'wrong'): Character[] {
+    const log = localStorage.getItem(type);
+    return log ? JSON.parse(log) : [];
+  }
+
+  addAnswerLog(type: 'correct' | 'wrong', character: Character): void {
+    const currentLog = this.getAnswerLog(type);
+    const updatedLog = [...currentLog, character];
+    localStorage.setItem(type, JSON.stringify(updatedLog));
+  }
 }
