19e03c3325784d74467d862d1ffaf378914cf640
bmi921 <bmi921gm@gmail.com>
2025-09-09 12:33:47 +0900

    feat: クイズの生成を実装、スクレイピングした

--- src/app/pages/quiz/quiz.module.ts
+++ src/app/pages/quiz/quiz.module.ts
@@ -1,4 +1,3 @@
-import { QuizCardComponent } from 'src/app/component/quiz-card/quiz-card.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -7,7 +6,7 @@ import { QuizRoutingModule } from './quiz-routing.module';
 import { QuizComponent } from './quiz.component';
 
 @NgModule({
-  declarations: [QuizComponent, QuizCardComponent],
+  declarations: [QuizComponent],
   imports: [CommonModule, QuizRoutingModule, FormsModule],
 })
 export class QuizModule {}
