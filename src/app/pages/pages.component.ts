4a8a3ca81d157a036ad2b09fe0de4f770c4c0a60
bmi921 <bmi921gm@gmail.com>
2025-09-10 17:37:40 +0900

    feat: スマホのサイドメニューが画面遷移時に戻るようにした

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.scss']
+  styleUrls: ['./pages.component.scss'],
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
   isSideMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        if (window.innerWidth < 768) {
+          this.isSideMenuOpen = false;
+        }
+      });
+  }
 
   toggleSideMenu(): void {
     this.isSideMenuOpen = !this.isSideMenuOpen;
