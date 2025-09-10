bde17d5d37d842df81a0abc852ec82a67f566c91
bmi921 <bmi921gm@gmail.com>
2025-09-10 14:17:43 +0900

    feat: キャラ一覧ページの実装

---
+++ src/app/pages/characters/characters.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Character } from '../../models/character.model';
+import { DataService } from '../../shared/data.service';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.scss'],
+})
+export class CharactersComponent implements OnInit {
+  characters$: Observable<Character[]>;
+  allCharacters: Character[] = [];
+  filteredCharacters: Character[] = [];
+  searchTerm: string = '';
+  prefectures: string[] = [];
+  selectedPrefecture: string = 'すべて';
+
+  constructor(private dataService: DataService) {
+    this.characters$ = this.dataService.getCharacters();
+  }
+
+  ngOnInit(): void {
+    this.characters$.subscribe(characters => {
+      this.allCharacters = characters;
+      this.filteredCharacters = characters;
+      this.prefectures = [
+        'すべて',
+        ...new Set(characters.map(c => c.prefecture)),
+      ];
+    });
+  }
+
+  search(): void {
+    let tempCharacters = this.allCharacters;
+
+    if (this.selectedPrefecture && this.selectedPrefecture !== 'すべて') {
+      tempCharacters = tempCharacters.filter(
+        character => character.prefecture === this.selectedPrefecture
+      );
+    }
+
+    if (this.searchTerm) {
+      tempCharacters = tempCharacters.filter(character =>
+        character.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
+
+    this.filteredCharacters = tempCharacters;
+  }
+}
---
+++ src/app/pages/characters/characters.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CharactersRoutingModule } from './characters-routing.module';
+import { CharactersComponent } from './characters.component';
+
+
+@NgModule({
+  declarations: [
+    CharactersComponent
+  ],
+  imports: [
+    CommonModule,
+    CharactersRoutingModule,
+    FormsModule
+  ]
+})
+export class CharactersModule { }
